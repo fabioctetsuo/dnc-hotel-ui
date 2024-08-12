506ce8530bf4df69905d5a9e1f30cfc12da91dc7
Fabio Tetsuo <fabioctetsuo@gmail.com>
2024-08-11 21:08:06 -0300

    build: update delivery groovy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,56 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from 'next-auth/providers/credentials'
-import axios from '@/api';
+import CredentialsProvider from "next-auth/providers/credentials";
+import axios from "@/api";
 import { cookies } from "next/headers";
 
 const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: 'E-mail', type: 'email' },
-                password: { label: 'Senha', type: 'password' },
-            },
-            async authorize(credentials) {
-                if (!credentials) throw new Error('no credentials!')
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "E-mail", type: "email" },
+        password: { label: "Senha", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) throw new Error("no credentials!");
 
-                const { data: { access_token } } = await axios.post('/auth/login', {
-                    email: credentials?.email,
-                    password: credentials?.password,
-                });
+        const {
+          data: { access_token },
+        } = await axios.post("/auth/login", {
+          email: credentials?.email,
+          password: credentials?.password,
+        });
 
-                cookies().set('access_token', access_token)
+        cookies().set("access_token", access_token);
 
-                const { sub: id } = JSON.parse(Buffer.from(access_token.split('.')[1], 'base64').toString());
-                
-                const { data: user } = await axios.get(`/users/${id}`, {
-                    headers: { Authorization: `Bearer ${access_token}` }
-                });
+        const { sub: id } = JSON.parse(
+          Buffer.from(access_token.split(".")[1], "base64").toString()
+        );
 
-                return {
-                    ...user,
-                    access_token,
-                    image: user.avatar,
-                }
-            },
-        })
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            return { ...token, ...user }
-        },
-        async session({ session, token }) {
-            session.user = token;
-            return session
-        }
+        const { data: user } = await axios.get(`/users/${id}`, {
+          headers: { Authorization: `Bearer ${access_token}` },
+        });
+
+        return {
+          ...user,
+          access_token,
+          image: user.avatar,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
     },
-    pages: { signIn: '/login' }
-}
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+  pages: { signIn: "/login" },
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,16 +2,19 @@ import NextAuth from "next-auth";
 import { Role } from "./User";
 
 declare module "next-auth" {
-    interface Session {
-        accessToken: string;
-        user: {
-            id: number,
-            email: string,
-            name: string,
-            role: Role,
-            avatar: string,
-            createdAt: string,
-            password: string
-        }
-    }
+  interface User {
+    access_token: string;
+  }
+  interface Session {
+    accessToken: string;
+    user: {
+      id: number;
+      email: string;
+      name: string;
+      role: Role;
+      avatar: string;
+      createdAt: string;
+      password: string;
+    };
+  }
 }
